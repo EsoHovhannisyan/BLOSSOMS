af94db2cf6afb35d3401f4998a0dc86546eeec90
EsoHovhannisyan <Hovhannisyan-11@bk.ru>
2024-05-23 22:47:05 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import './App.css';
 import Layout from './Layout/Layout';
 import Home from './Pages/Home/Home';
@@ -8,22 +9,31 @@ import Shop from './Pages/Shop/Shop';
 import Blog from './Pages/Blog/Blog';
 import NoPage from "./Pages/NoPage/NoPage";
 
+const ScrollToTop = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return null;
+};
+
 function App() {
   return (
-      <BrowserRouter basename='/'>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="About" element={<About />} />
-            <Route path="Contact" element={<Contact />} />
-            <Route path="Shop" element={<Shop />} />
-            <Route path="Blog" element={<Blog />} />
-            <Route path="*" element={<NoPage />} />
-            {/* <Route path="Sign" element={<Sign />} />
+    <BrowserRouter basename='/'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Shop" element={<Shop />} />
+          <Route path="Blog" element={<Blog />} />
+          <Route path="*" element={<NoPage />} />
+          {/* <Route path="Sign" element={<Sign />} />
             <Route path="Cart" element={<Cart />} /> */}
-          </Route>
-        </Routes>
-      </BrowserRouter>
+        </Route>
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
   );
 }
 export default App;
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,16 +3,16 @@ import Fashion from "./AboutComponents/Fashion/Fashion"
 import Mission from "./AboutComponents/Fashion/Mission/Mission"
 import Testimonials from "../Home/HomeComponents/Testimonials/Testimonials"
 import Icons from "../Home/HomeComponents/Icons/Icons"
+const About = () => {
 
-const About=()=>{
-return(
-    <main>
-        <LinkAbout />
-        <Fashion />
-        <Mission />
-        <Testimonials/>
-        <Icons />
-    </main>
-)
+    return (
+        <main>
+            <LinkAbout />
+            <Fashion />
+            <Mission />
+            <Testimonials />
+            <Icons />
+        </main>
+    )
 }
 export default About
